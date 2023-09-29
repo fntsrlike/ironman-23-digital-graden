3e205ee1d84418d86acab95a9515482751540f0f
Yuehu Lin <dev@fntsr.tw>
2023-09-29 21:50:13 +0800

    commit
    # feat: new features.
    # fix: bug fixes.
    # docs: documentation changes.
    # refactor: refactor of a particular code section without introducing new features or bug fixes.
    # style: code style improvements.
    # perf: performance improvements.
    # test: changes to the test suite.
    # ci: changes to the CI system.
    # build: changes to the build system (we don't yet have one so this shouldn't apply).
    # chore: for other changes that don't match previous types. This doesn't appear in the changelog.

--- components/TheArticle.vue
+++ components/TheArticle.vue
@@ -4,7 +4,16 @@
        <h1 class="tracking-tight font-extrabold text-gray-900 text-2xl leading-6 mb-1">
          {{ post.title }}
        </h1>
-       <!-- 預計放上發表時間 -->
+       <div class="text-sm text-gray-600 dark:text-gray-400">
+         <time
+           v-if="post.published_at"
+           :datetime="post.published_at"
+           :title="dateFormater(post.published_at, dateTimeFormat)"
+         >
+           發表於
+           {{ dateFormater(post.published_at, dateFormat) }}
+         </time>
+       </div>
      </header>
      <div class="my-4 break-word flex flex-col">
        <div class="mr-4">
@@ -12,18 +21,45 @@
        </div>
      </div>
      <footer class="pt-10">
-       <!-- 預計放上文章標籤 -->
+       <div v-if="post.tags?.length > 0">
+         <p class="inline mr-2">Tags:</p>
+         <ul class="inline">
+           <li v-for="tag in post.tags" :key="tag" class="inline">
+             <NuxtLink
+               :href="'/tags/' + kebabCase(tag)"
+               class="mr-3 text-sm font-medium uppercase text-teal-500 hover:text-teal-600 dark:hover:text-teal-400"
+             >
+               {{ tag.split(' ').join('-') }}
+             </NuxtLink>
+           </li>
+         </ul>
+       </div>
      </footer>
    </article>
 </template>
 <script setup lang="ts">
+import { DateTime } from 'luxon'
+import { kebabCase } from 'lodash-es/string'
 import { ParsedContent } from "@nuxt/content/dist/runtime/types"
 
 interface Post extends ParsedContent {
  title: string
+ title_en: string
+ slug: string
+ aliases: string[]
+ description: string
  created_at: string
  published_at: string
+ updated_at: string
  tags: string[]
+ draft: boolean
+}
+
+const dateFormat = 'yyyy-LL-dd'
+const dateTimeFormat = `${dateFormat} HH:mm`
+
+const dateFormater = (date: string, format: string) => {
+ return DateTime.fromISO(date).toLocal().toFormat(format)
 }
 
 defineProps<{
