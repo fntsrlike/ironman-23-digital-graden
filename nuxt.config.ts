6c1f7dad778df0d9b6562ddbf2da29b818bfa4be
Yuehu Lin <dev@fntsr.tw>
2023-10-06 17:00:27 +0800

    multiple sources

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
+import { resolve } from 'path'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   content: {
     markdown: {
       remarkPlugins: ['remark-breaks'],
     },
+    sources: {
+      obsidianSource: {
+        base: resolve(__dirname, '_write/content'),
+        driver: 'fs',
+      }
+    }
   },
 })
