c7de50ff5f5a8621db91849bbf631ae19641aa9d
Yuehu Lin <dev@fntsr.tw>
2023-10-10 16:51:42 +0800

    darkmode

--- app.vue
+++ app.vue
@@ -5,3 +5,15 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class:
+      'bg-white text-gray-900 dark:bg-neutral-900 dark:text-white',
+  },
+  htmlAttrs: {
+    lang: 'zh-Hant-TW',
+  },
+  title: '深藏若虛',
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-4xl xl:px-0 xl:text-lg">
     <div class="flex h-screen flex-col justify-between font-sans">
       <TheHeader />
-      <main class="mb-auto prose">
+      <main class="mb-auto prose dark:prose-invert">
         <slot />
       </main>
       <TheFooter />
