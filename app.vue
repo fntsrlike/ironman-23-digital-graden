a026d1f37aae6ea2e905913adf088fc59bf43b85
Yuehu Lin <dev@fntsr.tw>
2023-10-12 09:00:43 +0800

    move

--- app.vue
+++ app.vue
@@ -10,10 +10,6 @@ useHead({
   bodyAttrs: {
     class:
       'bg-white text-gray-700 dark:bg-neutral-900 dark:text-gray-300',
-  },
-  htmlAttrs: {
-    lang: 'zh-Hant-TW',
-  },
-  title: '深藏若虛',
+  }
 })
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,14 @@ import { resolve } from 'path'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'zh-Hant-TW',
+      },
+      title: 'My Digital Garden',
+    },
+  },
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
