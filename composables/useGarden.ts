68e559cb5be657e73a91a4d42f1043e87f5d6f2a
Yuehu Lin <dev@fntsr.tw>
2023-10-12 16:47:23 +0800

    useGarden

--- composables/useGarden.ts
+++ composables/useGarden.ts
@@ -1,10 +1,14 @@
 import { Post } from "@/types"
 import { startCase } from "@/libraries/formater"
 
-export function useGarden(post : Post|undefined = undefined  ) {
+function getTitleByRouteSlug() {
   const route = useRoute()
   const slug = route.name?.toString() || ''
-  const title = post?.title || startCase(slug) || ''
+  return startCase(slug)
+}
+
+export function useGarden(post : Post|undefined = undefined  ) {
+  const title = (post?.title || getTitleByRouteSlug() || '').replace(/^index$/i, '')
   const separator = title ? ' - ' : ''
 
   const headTitle = computed(() => `${title}${separator}My Digital Garden`)
@@ -12,6 +16,7 @@ export function useGarden(post : Post|undefined = undefined  ) {
   if (post) {
     post.head = post.head || {}
     post.head.title = post.head.title || headTitle
+    return
   }
 
   useHead({title: headTitle})
